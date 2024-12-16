bfdeab2a18f706d1509a1e9011a4f7f37acee4b7
unknown <carina.flor04@gmail.com>
2024-12-16 14:40:12 -0300

    Ajustando conexão com o banco para criar só uma EntityManagerFactory

--- ProjetoBancoDeDadosGeoespaciais/src/main/java/dao/Conexao.java
+++ ProjetoBancoDeDadosGeoespaciais/src/main/java/dao/Conexao.java
@@ -5,14 +5,28 @@ import jakarta.persistence.Persistence;
 
 public class Conexao {
 	
-	public EntityManagerFactory criarConexao() {
-	    EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("spatialite-test");
-		return entityFactory;
-	}
-	public void fecharConexao(EntityManagerFactory entityFactory) {
-		if (entityFactory.isOpen()){
-			entityFactory.close();
+	private static Conexao INSTANCIA;
+    private final EntityManagerFactory factory;
+
+    private Conexao() {
+    	this.factory = Persistence.createEntityManagerFactory("spatialite-test");
+    }
+    
+    public static Conexao getInstancia() {
+    	if(INSTANCIA == null) {
+    		INSTANCIA = new Conexao();
+    	}
+        return INSTANCIA;
+    }
+    
+	public void fecharConexao() {
+		if (factory.isOpen()){
+			factory.close();
 		}	
+	}
+
+	public EntityManagerFactory getFactory() {
+		return factory;
 	}
 
 }
