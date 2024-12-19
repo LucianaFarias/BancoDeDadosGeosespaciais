475967f4a4fe60d356ef93a118271b65c89aa359
unknown <carina.flor04@gmail.com>
2024-12-19 14:53:08 -0300

    Adicionando dao do pedido com função de listar pedidos

---
+++ ProjetoBancoDeDadosGeoespaciais/src/main/java/dao/IPedidoDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.util.List;
+
+import dto.PedidoDTO;
+
+public interface IPedidoDAO {
+	
+	public List<PedidoDTO> listarPedidos();
+
+}
---
+++ ProjetoBancoDeDadosGeoespaciais/src/main/java/dao/PedidoDAO.java
@@ -0,0 +1,51 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.EstoqueDTO;
+import dto.PedidoDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import mappers.MapperPedido;
+import model.Estoque;
+import model.Pedido;
+
+public class PedidoDAO implements IPedidoDAO{
+	
+	private EntityManagerFactory entityManagerFactory;
+	private MapperPedido mapper;
+
+	public PedidoDAO() {
+		this.entityManagerFactory = Conexao.getInstancia().getFactory();
+		this.mapper = new MapperPedido();
+	}
+
+	@Override
+	public List<PedidoDTO> listarPedidos() {
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+		List<Pedido> resultList;
+		try {
+			em.getTransaction().begin();
+			TypedQuery<Pedido> query = em.createQuery(
+					"SELECT e FROM Pedido", Pedido.class);
+
+			resultList = query.getResultList();
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+	    	throw e;
+	    }finally {
+	    	em.close();
+		}
+		
+		List<PedidoDTO> pedidos = new ArrayList<>();
+		for(Pedido pedido: resultList) {
+			pedidos.add(mapper.toDTO(pedido));
+		}
+		return pedidos;
+	}
+
+}
